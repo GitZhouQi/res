da4df7ee1accf95faf182283a6a69bcc572b8691
zhouqi <478930170@qq.com>
2018-05-15 17:48:51 +0800

    S

---
+++ DBTest/DBTest/sCollectionViewCell.h
@@ -0,0 +1,13 @@
+//
+//  sCollectionViewCell.h
+//  DBTest
+//
+//  Created by zhouqi on 2018/5/15.
+//  Copyright © 2018年 zhouqi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface sCollectionViewCell : UICollectionViewCell
+
+@end
