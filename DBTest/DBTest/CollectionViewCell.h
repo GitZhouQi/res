1a1a1ae0654ac245272b0ef592642f822f3e02eb
zhouqi <478930170@qq.com>
2018-05-15 16:48:53 +0800

    Commit

---
+++ DBTest/DBTest/CollectionViewCell.h
@@ -0,0 +1,13 @@
+//
+//  CollectionViewCell.h
+//  DBTest
+//
+//  Created by zhouqi on 2018/5/15.
+//  Copyright © 2018年 zhouqi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CollectionViewCell : UICollectionViewCell
+
+@end
---
+++ DBTest/DBTest/CollectionViewCell.m
@@ -0,0 +1,13 @@
+//
+//  CollectionViewCell.m
+//  DBTest
+//
+//  Created by zhouqi on 2018/5/15.
+//  Copyright © 2018年 zhouqi. All rights reserved.
+//
+
+#import "CollectionViewCell.h"
+
+@implementation CollectionViewCell
+
+@end
