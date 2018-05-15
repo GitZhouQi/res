16a19a5c963a507ae8ae506b099bfff4166ed3cd
zhouqi <478930170@qq.com>
2018-05-15 18:21:51 +0800

    Commit ss

---
+++ DBTest/DBTest/ssCollectionViewCell.h
@@ -0,0 +1,13 @@
+//
+//  ssCollectionViewCell.h
+//  DBTest
+//
+//  Created by zhouqi on 2018/5/15.
+//  Copyright © 2018年 zhouqi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ssCollectionViewCell : UICollectionViewCell
+
+@end
