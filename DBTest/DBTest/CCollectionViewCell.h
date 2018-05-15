db907053abc446db8878e3856680c7e1a650cda3
zhouqi <478930170@qq.com>
2018-05-15 18:25:38 +0800

    Sss

---
+++ DBTest/DBTest/CCollectionViewCell.h
@@ -0,0 +1,13 @@
+//
+//  CCollectionViewCell.h
+//  DBTest
+//
+//  Created by zhouqi on 2018/5/15.
+//  Copyright © 2018年 zhouqi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CCollectionViewCell : UICollectionViewCell
+
+@end
